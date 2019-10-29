fedb56d6374b407f1e3f19cfe3a3a379c6634355
KarasawaTakahiro <karasawa@basicinc.jp>
2019-10-29 22:59:20 +0900

    test: add tests for top, article index and article show

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,46 @@ fn rocket() -> rocket::Rocket {
 fn main() {
   rocket().launch();
 }
+
+#[cfg(test)]
+mod test {
+  use super::rocket;
+  use rocket::http::Status;
+  use rocket::local::Client;
+
+  #[test]
+  fn top() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.get("/").dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("Hello, world!".into()));
+  }
+
+  #[test]
+  fn articles() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.get("/articles").dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("article index".into()));
+  }
+
+  #[test]
+  fn article_1() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.get("/articles/1").dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("1".into()));
+  }
+
+  #[test]
+  fn article_2() {
+    let client = Client::new(rocket()).expect("valid rocket instance");
+    let mut response = client.get("/articles/2").dispatch();
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("2".into()));
+  }
+}
